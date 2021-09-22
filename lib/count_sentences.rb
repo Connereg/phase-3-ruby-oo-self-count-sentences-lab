70430581cb574f0c4d8a851e24d6237e84cb6322
Conner Gleason <connereg@optonline.net>
2021-09-22 19:38:49 -0400

    Completed Lab

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,33 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
+    self.end_with?("?")
 
   end
-
+ 
   def exclamation?
+    self.end_with?("!")
 
   end
 
   def count_sentences
+    #Regular Expression Solution
+    # self.split(/\.|\?|!/).filter { |sentence| !sentence.empty? }.size
 
+
+
+    sentences = 0
+    punctuation = [".", "?", "!"]
+
+    self.chars.each.with_index do |char, index|
+      if punctuation.include?(char) && !punctuation.include?(self[index + 1])
+        sentences += 1
+      end
+    end
+    sentences
   end
 end
